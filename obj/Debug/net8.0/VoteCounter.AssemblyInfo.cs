0704b4dd65f5143724c976c57ba80ae7204096de
KartofaKurti <krafter420@gmail.com>
2025-10-26 00:25:34 +0300

    Save local changes before pull

--- obj/Debug/net8.0/VoteCounter.AssemblyInfo.cs
+++ obj/Debug/net8.0/VoteCounter.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VoteCounter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c11d434ed40090d51f3e989e84ca87591d9c18d")]
 [assembly: System.Reflection.AssemblyProductAttribute("VoteCounter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VoteCounter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
